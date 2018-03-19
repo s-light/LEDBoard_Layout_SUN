24088175d02ae0a61fc3567ece53879c7e75672f
s-light <github@s-light.eu>
2018-03-19 11:24:52 +0100

    rename directories

--- sw/ETH_SPI_bridge/ola_config_spitest/config_send2target.sh
+++ sw/ETH_SPI_bridge/ola_config_spitest/config_send2target.sh
@@ -4,6 +4,6 @@ rootfs=~/mydata/acme/arietta/debian_8.10_jessie/target-rootfs
 target_config="$rootfs/home/light/.ola/*"
 
 # update local image
-cp "./target" $target_config
+cp "./target_config/*" $target_config
 # copy to remote device
 scp  $target_config "light@arietta.local:/home/light/.ola/"
